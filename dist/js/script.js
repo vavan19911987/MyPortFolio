d960e06fad8e00b66b373edaa49ef63bf93549f7
Vavan19911987 <vavan7520@gmail.com>
2021-09-20 18:53:46 +0300

    сделал адаптив всего сайта кроме политики ну как юы еще надо проверить сам сайт и каждый блог

--- dist/js/script.js
+++ dist/js/script.js
@@ -293,14 +293,14 @@ window.addEventListener('DOMContentLoaded', function () {
     // ! LOADER
 
 
-    // let mask = document.querySelector(".mask");
+    let mask = document.querySelector(".mask");
 
-    // window.addEventListener("load", function () {
-    //     mask.classList.add("hide");
-    //     setTimeout(function () {
-    //         mask.remove();
-    //     }, 600);
-    // });
+    window.addEventListener("load", function () {
+        mask.classList.add("hide");
+        setTimeout(function () {
+            mask.remove();
+        }, 600);
+    });
 
 
 
